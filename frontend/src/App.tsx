7aa6cb618c78756e03b57c1708f3604d3492af53
Ghianda22 <valelore98@gmail.com>
2023-01-23 15:17:04 +0100

    Added theme and fonts
    Created font-face in index.css file
    Setted the main font in the app component
    Created a theme object grouping all the project colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 
 const StyledApp = styled.div`
 	width: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+
+	font-family: "Poppins";
+	font-style: normal;
+	background-color: ${(props) => props.theme.colors.white};
 `;
 
 function App() {
 	return (
-		<StyledApp>
-			<div>components</div>
-			<div>components</div>
-		</StyledApp>
+		<ThemeProvider theme={theme}>
+			<StyledApp>
+				<div>components</div>
+				<div>components</div>
+			</StyledApp>
+		</ThemeProvider>
 	);
 }
 
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,31 @@
+const theme = {
+	colors: {
+		accentBlue100: "#554AF0",
+		accentBlue80: "#776EF3",
+		white: "#FFFFFF",
+		almostWhite: "#F8F8FA",
+		accentActive: "#2113E8",
+		secondaryActive: "#E2E3E4",
+		selector: "rgba(118, 118, 128, 0.12)",
+		separator: "rgba(60, 60, 67, 0.36);",
+		outline: "#D1D5DB",
+		outlineSecondary: "#D2D2D7",
+	},
+	text: {
+		color: {
+			dark100: "#04021D",
+			dark80: "#36354A",
+			dark60: "#686777",
+			accentBlue30: "#CCC9FB",
+		},
+	},
+};
+
+export default theme;
+
+/* --- SHORTCUTS ---
+
+${(props) => props.theme.colors.accentBlue100}
+
+
+*/
