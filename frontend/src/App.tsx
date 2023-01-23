402a7aabc10d1766c2892815c986138462e18f75
Ghianda22 <valelore98@gmail.com>
2023-01-23 10:58:15 +0100

    Cleaned the default config
    Installed styled-components
    Emptied App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<StyledApp>
+			<div>components</div>
+			<div>components</div>
+		</StyledApp>
+	);
 }
 
 export default App;
