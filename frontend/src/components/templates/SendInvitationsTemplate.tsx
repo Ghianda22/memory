909cd188b1e4efd2075cf2b00aa0871a028e5795
Ghianda22 <valelore98@gmail.com>
2023-02-21 16:21:08 +0100

    Other few fixes

--- frontend/src/components/templates/SendInvitationsTemplate.tsx
+++ frontend/src/components/templates/SendInvitationsTemplate.tsx
@@ -1,4 +1,4 @@
-import {  useContext } from "react";
+import { useContext } from "react";
 import styled from "styled-components";
 import { images } from "../../data/imagesData";
 import { CreateGameContext } from "../../pages/CreateGame";
@@ -11,37 +11,48 @@ const FullPage = styled.div`
 	display: flex;
 	height: 100%;
 	flex-direction: column;
+	justify-content: space-between;
 `;
 
 export default function SendInvitationsTemplate() {
-  const { sendInvitations, next } = useContext(CreateGameContext);
-  
-  let avatarIcons: string[] = images.icon;
-  avatarIcons = avatarIcons.filter((icon) => icon !== sendInvitations.pageData.unavailableAvatar);
-  
-  const numOfInvitations = parseInt(sendInvitations.pageData.numberOfPlayers) -1;
+	const { sendInvitations, next } = useContext(CreateGameContext);
 
-  
+	let avatarIcons: string[] = images.icon;
+	avatarIcons = avatarIcons.filter(
+		(icon) => icon !== sendInvitations.pageData.unavailableAvatar
+	);
 
-  const renderInputs: JSX.Element[] = [];
-		for (let i = 0; i < numOfInvitations; i++) {
-      renderInputs[i] =
-        <>
-          <LabelText>{sendInvitations.pageData.label + " " + (i + 1)}</LabelText>
-          <TextInput
-            image={avatarIcons[i]}
-            placeholder={sendInvitations.pageData.placeholder}
-            fieldName={`${i}`}
-            handleOnChange={sendInvitations.updateState}
-          />
-        </>;
-  };
+	const numOfInvitations =
+		parseInt(sendInvitations.pageData.numberOfPlayers) - 1;
+
+	const renderInputs: JSX.Element[] = [];
+	for (let i = 0; i < numOfInvitations; i++) {
+		renderInputs[i] = (
+			<>
+				<LabelText>
+					{sendInvitations.pageData.label + " " + (i + 1)}
+				</LabelText>
+				<TextInput
+					image={avatarIcons[i]}
+					placeholder={sendInvitations.pageData.placeholder}
+					fieldName={`${i}`}
+					handleOnChange={sendInvitations.updateState}
+				/>
+			</>
+		);
+	}
 
 	return (
 		<FullPage>
-			<Title>{sendInvitations.pageData.title}</Title>
-      {renderInputs}
-      <Button buttonStyle={"normal"} text={sendInvitations.pageData.button} onClickHandler={next}/>
+			<div>
+				<Title>{sendInvitations.pageData.title}</Title>
+				{renderInputs}
+			</div>
+			<Button
+				buttonStyle={"normal"}
+				text={sendInvitations.pageData.button}
+				onClickHandler={next}
+			/>
 		</FullPage>
 	);
 }
