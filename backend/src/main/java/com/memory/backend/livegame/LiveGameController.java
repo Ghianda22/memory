bf3bfc3b78ca026ce4f2660c594c832f77772686
Ghianda22 <valelore98@gmail.com>
2023-03-26 19:19:44 +0200

    Started defining the core functionalities of the game

---
+++ backend/src/main/java/com/memory/backend/livegame/LiveGameController.java
@@ -0,0 +1,7 @@
+package com.memory.backend.livegame;
+
+public class LiveGameController {
+    public void startGame(){}
+    public void quitGame(){}
+    public void enterGame(){}
+}
---
+++ backend/src/main/java/com/memory/backend/livegame/LiveGameService.java
@@ -0,0 +1,7 @@
+package com.memory.backend.livegame;
+
+public class LiveGameService {
+    public void calculateRanking(){}
+    public void changeTurnPlayer(){}
+    public void doCardsMatch(){}
+}
