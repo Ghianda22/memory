b9c0bb4ecee4f0ac6bb857b2af2dc905e4a6953b
Ghianda22 <valelore98@gmail.com>
2023-04-03 11:41:40 +0200

    Implemented basic connection with websocket

--- frontend/src/pages/LiveGame.tsx
+++ frontend/src/pages/LiveGame.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import SockJS from "sockjs-client";
+import Stomp, { Client } from "stompjs";
 
+const wsURL = "http://localhost:8080";
+
+interface MessageReceived {
+	from: string;
+	text: string;
+}
+
+let stompClient: Client;
 export default function LiveGame() {
-  return (
-    <div>LiveGame</div>
-  )
+	const [messages, setMessages] = useState<MessageReceived[]>([]);
+
+	useEffect(() => {
+		connect();
+	}, []);
+
+	function connect() {
+		var socket = new SockJS(wsURL+"/chat");
+		stompClient = Stomp.over(socket);
+		stompClient.connect({}, function (frame) {
+			stompClient.subscribe("/topic/messages", function (messageOutput) {
+				showMessageOutput(JSON.parse(messageOutput.body));
+			});
+		});
+	}
+
+	function sendMessage() {
+		stompClient.send(
+			"/app/chat",
+			{},
+			JSON.stringify({ from: "from", text: "testo di prova" })
+		);
+	}
+
+	function showMessageOutput(messageOutput: MessageReceived) {
+		console.log("Questo è il messaggio " + messageOutput);
+    
+  }
+
+	return (
+		<div>
+			LiveGame
+			<button onClick={sendMessage}>Send message</button>
+		</div>
+	);
 }
